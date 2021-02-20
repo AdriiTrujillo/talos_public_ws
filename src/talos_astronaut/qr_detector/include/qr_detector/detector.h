b69521b1bbd31fce79d0a3517c40037bf1af3e92
AdriiTrujillo <adrii.trujillo@gmail.com>
2021-02-20 18:51:12 +0100

    Qr detector

---
+++ src/talos_astronaut/qr_detector/include/qr_detector/detector.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <opencv/cv.h>
+#include "zbar.h"
+
+namespace qr_detector {
+
+struct Tag {
+  std::string message;
+  std::vector<cv::Point> polygon;
+};
+
+using Tags = std::vector<Tag>;
+
+/**
+ * Main QR codes detector class
+ */
+class Detector {
+ public:
+  Detector();
+  ~Detector() = default;
+
+  /**
+   * Detects tags in image.
+   *
+   * @param[in] image - image with qr tags
+   * @param[in] timeout - max time for tags detection in ms
+   */
+  Tags detect(const cv::Mat& image, size_t timeout = 100);
+
+ protected:
+  zbar::ImageScanner scanner_;
+};
+
+}

d50d9aba3e7253887629fb1d79aed6345c22a0b7
AdriiTrujillo <adrii.trujillo@gmail.com>
2021-03-02 10:48:26 +0100

    Aruco Controller

--- src/talos_astronaut/astronaut_controllers/src/cartesian_controller.cpp
+++ src/talos_astronaut/astronaut_controllers/src/cartesian_controller.cpp
@@ -1,12 +1,12 @@
 
 #include <pluginlib/class_list_macros.h>
-#include <astronaut_controllers/cartesian_controller.h>
+#include <astronaut_controllers/aruco_cartesian_controller.h>
 
 namespace position_controller {
 
-typedef controller_ns::cartesian_controller_class PositionController;
+typedef controller_ns::aruco_cartesian_controller_class ArucoController;
 
 };
 
-PLUGINLIB_EXPORT_CLASS(position_controller::PositionController, controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(position_controller::ArucoController, controller_interface::ControllerBase);
 
